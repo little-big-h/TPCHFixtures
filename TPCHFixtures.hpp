42a0b5f8994e2687cedc98dc02d008d3a4a39fcd
Holger <holger@csail.mit.edu>
2016-10-03 14:16:35 -0400

    added header

---
+++ TPCHFixtures.hpp
@@ -0,0 +1,17 @@
+#ifndef TPCHFIXTURES_H
+#define TPCHFIXTURES_H
+#include <string>
+
+namespace TPCH {
+namespace Fixtures {
+std::string customerData;
+std::string nationData;
+std::string ordersData;
+std::string partData;
+std::string partsuppData;
+std::string regionData;
+std::string supplierData;
+} // Fixtures
+} // TPCH
+
+#endif /* TPCHFIXTURES_H */
